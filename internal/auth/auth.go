cdf1c13b5ff438394189609a3ab02726351f51f4
galikp <pal.galik@schibsted.com>
2019-10-20 12:16:51 +0200

    Add authenticator package

---
+++ internal/auth/auth.go
@@ -0,0 +1,69 @@
+package auth
+
+import (
+	"encoding/json"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/galikpali/awesome-blog-engine/internal/config"
+	"github.com/galikpali/awesome-blog-engine/internal/user"
+	"golang.org/x/crypto/bcrypt"
+	"net/http"
+	"time"
+)
+
+type Credentials struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type Claims struct {
+	Email string `json:"email"`
+	jwt.StandardClaims
+}
+
+var jwtKey = []byte(config.GetConfig().JWT.Secret)
+
+func Auth(w http.ResponseWriter, r *http.Request) {
+	var credentials Credentials
+	err := json.NewDecoder(r.Body).Decode(&credentials)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	userEntity := user.FindByEmail(credentials.Email)
+
+	if !checkPasswordHash(credentials.Password, userEntity.PasswordHash) {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	addTokenToResponseHeader(credentials, err, w)
+}
+
+func addTokenToResponseHeader(credentials Credentials, err error, w http.ResponseWriter) {
+	expirationTime := time.Now().Add(5 * time.Minute)
+	claims := &Claims{
+		Email: credentials.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+}
+
+func checkPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
